b491946955ffac9b4f058354fe2fe841bfbf5cfd
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2024-10-10 15:28:08 +0530

    Update dashboard component

--- src/layouts/dashboard/components/OrdersOverview/index.js
+++ src/layouts/dashboard/components/OrdersOverview/index.js
@@ -29,7 +29,7 @@ function OrdersOverview() {
     <Card sx={{ height: "100%" }}>
       <MDBox pt={3} px={3}>
         <MDTypography variant="h6" fontWeight="medium">
-          Orders overview
+          Cusdec Tracking
         </MDTypography>
         <MDBox mt={0} mb={2}>
           <MDTypography variant="button" color="text" fontWeight="regular">
@@ -38,9 +38,9 @@ function OrdersOverview() {
             </MDTypography>
             &nbsp;
             <MDTypography variant="button" color="text" fontWeight="medium">
-              24%
+              status -
             </MDTypography>{" "}
-            this month
+            Successful
           </MDTypography>
         </MDBox>
       </MDBox>
@@ -48,31 +48,28 @@ function OrdersOverview() {
         <TimelineItem
           color="success"
           icon="notifications"
-          title="$2400, Design changes"
+          title="Exit Note"
           dateTime="22 DEC 7:20 PM"
         />
         <TimelineItem
           color="error"
           icon="inventory_2"
-          title="New order #1832412"
-          dateTime="21 DEC 11 PM"
+          title="Release Order"
+          dateTime="24 DEC 11 PM"
         />
-        <TimelineItem
+        <TimelineItem color="info" icon="payment" title="Payment" dateTime="20 DEC 2:20 AM" />
+
+        {/* <TimelineItem
           color="info"
           icon="shopping_cart"
           title="Server payments for April"
           dateTime="21 DEC 9:34 PM"
-        />
-        <TimelineItem
-          color="warning"
-          icon="payment"
-          title="New card added for order #4395133"
-          dateTime="20 DEC 2:20 AM"
-        />
+        /> */}
+
         <TimelineItem
           color="primary"
           icon="vpn_key"
-          title="New card added for order #4395133"
+          title="Register"
           dateTime="18 DEC 4:54 AM"
           lastItem
         />
